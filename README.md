6f843f2c0c7f3afa082b382256bf85ee0fbd3081
quagmire_toilet_code <54505582+uyigiug@users.noreply.github.com>
2025-11-20 14:10:43 +0000

    Adiciona os scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews.Zekai
+
 Apenas um projeto a qual busco tentar refazer as linhas de código do tabnews e assim aprender programação.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
-function  Home() {
-    return <h1>Hi, i see you from the webcam 👀. And i know you are from chique-chique Bahia📍.</h1>
+function Home() {
+  return (
+    <h1>
+      Hi, i see you from the webcam 👀. And i know you are from chique-chique
+      Bahia📍.
+    </h1>
+  );
+}
+function teste() {
+  console.log("teste");
+}
+function teste2() {
+  console.log("identação errada");
 }
 
-export default Home; 
+export default Home;
